35afaacac1e0a812f9503eed4bb501c31a29f436
Mikhail <unknxwnuser5078@gmail.com>
2026-01-24 17:22:50 +0300

    Автопроверка: ввод для параметра "firstName" при создании пользователя

--- test_user_create.py
+++ test_user_create.py
@@ -8,24 +8,17 @@ def get_user_body(first_name):
 def positive_assert(first_name):
     user_body = get_user_body(first_name)
     user_response = sender_stand_request.post_new_user(user_body)
-    
     assert user_response.status_code == 201
     assert user_response.json()["authToken"] != ""
-    
     users_table_response = sender_stand_request.get_users_table()
-    
     str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
                    + user_body["address"] + ",,," + user_response.json()["authToken"]
-    
     assert users_table_response.text.count(str_user) == 1
     
 def negative_assert_symbol(first_name):
     user_body = get_user_body(first_name)
-    
     response = sender_stand_request.post_new_user(user_body)
-    
     assert response.status_code == 400
-    
     assert response.json()["code"] == 400
     assert response.json()["message"] == "Имя пользователя введено некорректно. " \
                                              "Имя может содержать только русские или латинские буквы, " \
@@ -33,9 +26,7 @@ def negative_assert_symbol(first_name):
     
 def negative_assert_no_firstname(user_body):
     response = sender_stand_request.post_new_user(user_body)
-    
     assert response.status_code == 400
-    
     assert response.json()["code"] == 400
     assert response.json()["message"] == "Не все необходимые параметры были переданы"
     
@@ -78,9 +69,5 @@ def test_create_user_empty_first_name_get_error_response():
 def test_create_user_number_type_first_name_get_error_response():
     user_body = get_user_body(12)
     response = sender_stand_request.post_new_user(user_body)
-    
     assert response.status_code == 400
-    
-    
-    
     
